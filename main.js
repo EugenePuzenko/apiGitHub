738fb1443c917e82b5011df2e5c0574454cd3449
EugenePuzenko <96841712+EugenePuzenko@users.noreply.github.com>
2022-05-24 14:44:07 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const autocompleteList = document.getElementById('autocompleteList')
 const selectedList = document.getElementById('selectedList')
 const selectedItems = selectedList.children
 const autocompleteListItems = autocompleteList.children
+const url = "https://api.github.com/search/repositories"
 
 class Autocomplete {
   constructor(data) {
@@ -11,7 +12,7 @@ class Autocomplete {
 
   showFirstFiveRepositories(repo){
     repo.forEach(function (repo) {
-      let li = document.createElement('li')
+      const li = document.createElement('li')
       li.classList.add('item')
       li.innerHTML = repo.name
       autocompleteList.appendChild(li)
@@ -19,26 +20,17 @@ class Autocomplete {
   }
   
   addToSelectedList(data){
-    let div = document.createElement('div')
-    let textBlock = document.createElement('div')
-    let name = document.createElement('p')
-    let owner = document.createElement('p')
-    let stars = document.createElement('p')
-    let close = document.createElement('button')
-    div.classList.add('selected-item')
-    name.classList.add('selected-text')
-    owner.classList.add('selected-text')
-    stars.classList.add('selected-text')
-    close.classList.add('selected-delete')
-    textBlock.classList.add('text-block')
-    name.innerHTML = `Name: ${data.name}`
-    owner.innerHTML = `Owner: ${data.owner.login}`
-    stars.innerHTML = `Stars: ${data.stargazers_count}`
-    textBlock.appendChild(name)
-    textBlock.appendChild(owner)
-    textBlock.appendChild(stars)
-    div.appendChild(textBlock)
-    div.appendChild(close)
+    const template = 
+    `<div class='selected-item'>
+      <div class='text-block'>
+        <p class='selected-text'>Name: ${data.name}</p>
+        <p class='selected-text'>Owner: ${data.owner.login}</p>
+        <p class='selected-text'>Stars: ${data.stargazers_count}</p>
+      </div>
+      <button class='selected-delete'></button>
+    </div>`
+    const div = document.createElement('div')
+    div.innerHTML = template
     document.getElementById('selectedList').appendChild(div)
   }
 }
@@ -46,12 +38,10 @@ class Autocomplete {
 const autocomplete = new Autocomplete();
 
 async function getRepo(userText) {
-  const repoResponse = await fetch(`https://api.github.com/search/repositories?q=${userText}&sort=stars&per_page=5`);
+  const repoResponse = await fetch(`${url}?q=${userText}&sort=stars&per_page=5`);
   if (repoResponse.status === 200) {
     const repo = await repoResponse.json();
     return [...repo.items]
-  } else {
-    return
   }
 }
 
@@ -65,17 +55,15 @@ const debounce = (fn, ms) => {
 }
 
 function handleInput(e) {
-  if (input.value === '') {
+  if (!input.value) {
     autocompleteList.innerHTML = '';
     return
   }
   
   getRepo(e.target.value).then(data => {
-    if (e.target.value != '') {
-      autocompleteList.innerHTML = '';
-      autocomplete.data = data;
-      autocomplete.showFirstFiveRepositories(data);
-    }
+    autocompleteList.innerHTML = '';
+    autocomplete.data = data;
+    autocomplete.showFirstFiveRepositories(data);
   })
 }
 
